cec9e80c09ca4dadcf4e6dde18ddd454395420cc
Simone-Ricc <simonericcio02@gmail.com>
2021-06-07 22:18:59 +0200

    previsione

--- grafici/future_previsioni.js
+++ grafici/future_previsioni.js
@@ -2,7 +2,7 @@ function getDati1(){
 	var datas1; 
 	$.ajax({
 		async: false,
-		url: "https://api.elaborato-covid19.com/datiNazionaliNuoviPos",
+		url: "https://api.elaborato-covid19.com/previsioniCasiGiornalieri",
 		success: function(dati){	
 		datas1 = dati;	
 		return dati;
@@ -15,14 +15,9 @@ function getDati2(){
 	var datas2; 
 	$.ajax({
 		async: false,
-		url: "https://api.elaborato-covid19.com/datiNazionaliDeceduti",
+		url: "https://api.elaborato-covid19.com/previsioniCasi",
 		success: function(dati){	
 		datas2 = dati;
-
-		for(var i=0;i<datas2.length-1;i++){	//per ogni elemento faccio la differenza, eccetto l'ultimo (che non verrà visualizzato)
-			datas2[i].Deceduti  = datas2[i].Deceduti-datas2[i+1].Deceduti;	//differenza
-		}
-		datas2.pop();	//rimuovo l'ultimo elemento (non posso sottrarlo a quello dopo essendo l'ultimo e allora non lo visualizzo)
 		return datas2;
 	}});
 
@@ -33,15 +28,9 @@ function getDati3(){
 	var datas3; 
 	$.ajax({
 		async: false,
-		url: "https://api.elaborato-covid19.com/datiNazionaliDimessi",
+		url: "https://api.elaborato-covid19.com/previsioniMorti",
 		success: function(dati){	
 		datas3 = dati;
-		
-		for(var i=0;i<datas3.length-1;i++){	//per ogni elemento faccio la differenza, eccetto l'ultimo (che non verrà visualizzato)
-			datas3[i].Dimessi  = datas3[i].Dimessi-datas3[i+1].Dimessi;	//differenza
-		}
-		datas3.pop();	//rimuovo l'ultimo elemento (non posso sottrarlo a quello dopo essendo l'ultimo e allora non lo visualizzo)
-		
 		
 		return dati;
 	}});
@@ -61,68 +50,84 @@ window.onload = function() {
 
 	//var labelsA = labels.replace("T00:00:00.000Z", "");
 	
-	var data = datas1.map(function(e){
-		return e.NuoviPositivi;
+	var data1 = datas1.map(function(e){
+		return e.Valore;
 	});
+
+	var data2 = datas1.map(function(e){
+		return e.ValoreMin;
+	});
+
+
+	var data3 = datas1.map(function(e){
+		return e.ValoreMax;
+	});
+
 
 	var ctx1 = myChart1.getContext('2d');
 
-
-window.boh = {
-
-
-responsive: true,
-type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-data:{
-	labels:labels.reverse(),
-	datasets:[{
-	label:'Previsioni Casi Giornalieri',
-	data: data.reverse(),
-	backgroundColor:'rgba(170, 2, 2, 0.6)',
-	borderColor: '#a80d0d',
-	borderWidth:3.5,
-	hoverBorderWidth:3,
-	hoverBorderColor:'#b00909',
-	fill: false,
-	lineTension: 0,
-	}]
-},
-options:{
-	scales:{
-		yAxes:[{
-			ticks:{
-				beginAtZero: true
+	window.boh = {
+	  type: "bar",
+	  data: {
+		labels: labels,
+		datasets: [
+		  {
+			label: "Line",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 192, 0.5)",
+			borderColor: "rgb(75, 192, 192)",
+			hoverBorderColor: "rgb(175, 192, 192)",
+			fill: false,
+			tension: 0,
+			data: data1,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  },
+		  {
+			label: "BandTop",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 255, 0.5)",
+			borderColor: "transparent",
+			pointRadius: 0,
+			fill: 0,
+			tension: 0,
+			data: data3,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  },
+		  {
+			label: "BandBottom",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 255, 0.5)",
+			borderColor: "transparent",
+			pointRadius: 0,
+			fill: 0,
+			tension: 0,
+			data: data2,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  }
+		  
+		]
+	  },
+	  options: {
+		scales: {
+		  xAxes: [{
+			id: 'x',
+			type: 'category'
+		  }],
+		  yAxes: [{
+			id: 'y',
+			type: 'linear',
+			position: "left",
+			ticks: {
+			  stepSize: 10
 			}
-			
-		}]
-	},
-	title:{
-	display:true,
-	text:'Previsioni Casi giornalieri',
-	fontSize:30,
-	},
-	legend:{
-	borderWidth:1,
-	display:true,
-	position:'bottom',
-	labels:{
-		fontColor:'#000'
-	}
-	},
-	layout:{
-	padding:{
-		left:50,
-		right:50,
-		bottom:10,
-		top:10
-	}
-	},
-	tooltips:{
-	enabled:true
-	},
+		  }]
+		}
+	  }
+	};
 	
-}
-};
 
 var chart1 = new Chart(ctx1, boh)
 
@@ -131,71 +136,84 @@ var datas2 = getDati2();
 
 	console.log(datas2);
 	
-	var labels = datas2.map(function(e){
-		return e.Data.replace("T00:00:00.000Z", "");
+	var data1 = datas2.map(function(e){
+		return e.Valore;
 	});
-	
-	var data = datas2.map(function(e){
-		return e.Deceduti;
+
+	var data2 = datas2.map(function(e){
+		return e.ValoreMin;
 	});
+
+
+	var data3 = datas2.map(function(e){
+		return e.ValoreMax;
+	});
+
 
 	var ctx2 = myChart2.getContext('2d');
 
 
-window.boh2 = {
-
-responsive: true,
-type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-data:{
-	labels:labels.reverse(),
-	datasets:[{
-	label:'Previsioni Morti Giornalieri',
-	data: data.reverse(),
-	backgroundColor:'rgba(0, 0, 0, 0.6)',
-	borderColor: '#000',
-	borderWidth:3.5,
-	hoverBorderWidth:3,
-	hoverBorderColor:'#b00909',
-	fill: false,
-	lineTension: 0,
-	}]
-},
-options:{
-	scales:{
-		yAxes:[{
-			ticks:{
-				beginAtZero: true
+	window.boh2 = {
+		type: "bar",
+	  data: {
+		labels: labels,
+		datasets: [
+		  {
+			label: "Line",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 192, 0.5)",
+			borderColor: "rgb(75, 192, 192)",
+			hoverBorderColor: "rgb(175, 192, 192)",
+			fill: false,
+			tension: 0,
+			data: data1,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  },
+		  {
+			label: "BandTop",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 255, 0.5)",
+			borderColor: "transparent",
+			pointRadius: 0,
+			fill: 0,
+			tension: 0,
+			data: data3,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  },
+		  {
+			label: "BandBottom",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 255, 0.5)",
+			borderColor: "transparent",
+			pointRadius: 0,
+			fill: 0,
+			tension: 0,
+			data: data2,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  }
+		  
+		]
+	  },
+	  options: {
+		scales: {
+		  xAxes: [{
+			id: 'x',
+			type: 'category'
+		  }],
+		  yAxes: [{
+			id: 'y',
+			type: 'linear',
+			position: "left",
+			ticks: {
+			  stepSize: 15
 			}
-			
-		}]
-	},
-	title:{
-	display:true,
-	text:'Previsioni Morti Giornalieri',
-	fontSize:30,
-	},
-	legend:{
-	borderWidth:1,
-	display:true,
-	position:'bottom',
-	labels:{
-		fontColor:'#000'
-	}
-	},
-	layout:{
-	padding:{
-		left:50,
-		right:50,
-		bottom:10,
-		top:10
-	}
-	},
-	tooltips:{
-	enabled:true
-	},
-	
-}
-};
+		  }]
+		}
+	  }
+	};
 
 var chart2 = new Chart(ctx2, boh2)
 
@@ -204,72 +222,82 @@ var datas3 = getDati3();
 
 	console.log(datas3);
 	
-	var labels = datas3.map(function(e){
-		return e.Data.replace("T00:00:00.000Z", "");
-	});
-	
-	var data = datas3.map(function(e){
-		return e.Dimessi;
+	var data1 = datas3.map(function(e){
+		return e.Valore;
 	});
 
+	var data2 = datas3.map(function(e){
+		return e.ValoreMin;
+	});
+
+
+	var data3 = datas3.map(function(e){
+		return e.ValoreMax;
+	});
 	var ctx3 = myChart3.getContext('2d');
 
 
-window.boh3 = {
-
-
-responsive: true,
-type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-data:{
-	labels:labels.reverse(),
-	datasets:[{
-	label:'Previsioni Guariti Giornalieri',
-	data: data.reverse(),
-	backgroundColor:'rgba(24, 163, 11, 0.6)',
-	borderColor: '#18a30b',
-	borderWidth:3.5,
-	hoverBorderWidth:3,
-	hoverBorderColor:'#18a30b',
-	fill: false,
-	lineTension: 0,
-	}]
-},
-options:{
-	scales:{
-		yAxes:[{
-			ticks:{
-				beginAtZero: true
+	var boh3 = {
+		type: "bar",
+	  data: {
+		labels: labels,
+		datasets: [
+		  {
+			label: "Line",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 192, 0.5)",
+			borderColor: "rgb(75, 192, 192)",
+			hoverBorderColor: "rgb(175, 192, 192)",
+			fill: false,
+			tension: 0,
+			data: data1,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  },
+		  {
+			label: "BandTop",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 255, 0.5)",
+			borderColor: "transparent",
+			pointRadius: 0,
+			fill: 0,
+			tension: 0,
+			data: data3,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  },
+		  {
+			label: "BandBottom",
+			type: "line",
+			backgroundColor: "rgb(75, 192, 255, 0.5)",
+			borderColor: "transparent",
+			pointRadius: 0,
+			fill: 0,
+			tension: 0,
+			data: data2,
+			yAxisID: 'y',
+			xAxisID: 'x'
+		  }
+		  
+		]
+	  },
+	  options: {
+		scales: {
+		  xAxes: [{
+			id: 'x',
+			type: 'category'
+		  }],
+		  yAxes: [{
+			id: 'y',
+			type: 'linear',
+			position: "left",
+			ticks: {
+			  stepSize: 10
 			}
-			
-		}]
-	},
-	title:{
-	display:true,
-	text:'Previsioni Guariti Giornalieri',
-	fontSize:30,
-	},
-	legend:{
-	borderWidth:1,
-	display:true,
-	position:'bottom',
-	labels:{
-		fontColor:'#000'
-	}
-	},
-	layout:{
-	padding:{
-		left:50,
-		right:50,
-		bottom:10,
-		top:10
-	}
-	},
-	tooltips:{
-	enabled:true
-	},
-	
-}
-};
+		  }]
+		}
+	  }
+	};
 
 var chart3 = new Chart(ctx3, boh3)
 
