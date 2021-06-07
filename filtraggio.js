d17afa1e33343058fa2960cf95c3a62360277bfb
Simone-Ricc <simonericcio02@gmail.com>
2021-06-07 21:51:01 +0200

    filtro completo

--- filtraggio.js
+++ filtraggio.js
@@ -34,5 +34,73 @@ function filtra(){
     console.log(ritorno);
 
 
+    var labels = ritorno.map(function(e){
+		return e.Data.replace("T00:00:00.000Z", "");
+	});
+	
+	var data = ritorno.map(function(e){
+		return e.Dosi;
+	});
+    
+	var ctx1 = myChart1.getContext('2d');
+
+    var boh = {
+        responsive: true,
+        type:'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data:{
+            labels:labels.reverse(),
+            datasets:[{
+            label:'Vaccini giornalieri',
+            data:data.reverse(),
+            backgroundColor:'rgba(65, 105, 255, 0.6)',
+            borderColor: '#4169e1',
+            borderWidth:3.5,
+            hoverBorderWidth:3,
+            hoverBorderColor:'#4169e1',
+            fill: false,
+            lineTension: 0,
+            }]
+        },
+        options:{
+            scales:{
+                yAxes:[{
+                    ticks:{
+                        beginAtZero: true
+                    }
+                    
+                }]
+            },
+            title:{
+            display:true,
+            text:'Vaccini giornalieri',
+            fontSize:30,
+            },
+            legend:{
+            borderWidth:1,
+            display:true,
+            position:'bottom',
+            labels:{
+                fontColor:'#000'
+            }
+            },
+            layout:{
+            padding:{
+                left:50,
+                right:50,
+                bottom:10,
+                top:10
+            }
+            },
+            tooltips:{
+            enabled:true
+            },
+            
+        }
+        };
+        var chart1 = new Chart(ctx1, boh)
+
+        document.getElementById("myChart1").style=""
+
+        ctx1.clearRect(0, 0, myChart1.width, myChart1.height);
 }
 
